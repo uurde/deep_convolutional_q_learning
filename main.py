9ce2883d85e679d9f721a8a33df1d5266874ee2a
Uğur Değirmenci <m.ugurdegirmenci@gmail.com>
2024-12-06 15:15:57 +0300

    implementing and initializing the dcqn class

--- main.py
+++ main.py
@@ -66,3 +66,49 @@ def preprocessing_frame(frame):
     frame = Image.fromarray(frame)
     preprocess = transforms.Compose([transforms.Resize((128, 128)), transforms.ToTensor()])
     return preprocess(frame).unsqueeze(0)
+
+#implementing the dcqn class
+class Agent():
+    def __init__(self, action_size):
+        self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+        self.action_size = action_size
+        self.local_qnetwork = Network(action_size).to(self.device)
+        self.target_qnetwork = Network(action_size).to(self.device)
+        self.optimizer = optim.Adam(self.local_qnetwork.parameters(), lr = learning_rate)
+        self.memory = deque(maxlen = 10000)
+    
+    def step(self, state, action, reward, next_state, done):
+        state = preprocessing_frame(state)
+        next_state = preprocessing_frame(next_state)
+        if len(self.memory) > minibatch_size:
+            experiences = random.sample(self.memory, k = minibatch_size)
+            self.learn(experiences, discount_factor)
+    
+    def act(self, state, epsilon = 0.):
+        state = preprocessing_frame(state).to(self.device)
+        self.local_qnetwork.eval()
+        with torch.no_grad():
+            action_values = self.local_qnetwork(state)
+        self.local_qnetwork.train()
+        if random.random() > epsilon:
+            return np.argmax(action_values.cpu().data.numpy())
+        else:
+            return random.choice(np.arange(self.action_size))
+        
+    def learn(self, experiences, discount_factor):
+        states, actions, rewards, next_states, dones = zip(*experiences)
+        states = torch.from_numpy(np.vstack(states)).float().to(self.device)
+        actions = torch.from_numpy(np.vstack(actions)).long().to(self.device)
+        rewards = torch.from_numpy(np.vstack(rewards)).float().to(self.device)
+        next_states = torch.from_numpy(np.vstack(next_states)).float().to(self.device)
+        dones = torch.from_numpy(np.vstack(dones).astype(np.uint8)).float().to(self.device)
+        next_q_states = self.target_qnetwork(next_states).detach().max(1)[0].unsqueeze(1)
+        q_targets = rewards + discount_factor * next_q_states * (1 - dones)
+        q_expected = self.local_qnetwork(states).gather(1, actions)
+        loss = F.mse_loss(q_expected, q_targets)
+        self.optimizer.zero_grad()
+        loss.backward()
+        self.optimizer.step()
+
+#initializing the qcqn agent
+agent = Agent(number_actions)
